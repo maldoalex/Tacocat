7e4ba96aff8b9b5c7e9fe1dcb4605c08f9f646b3
maldoalex <maldoalex@gmail.com>
2021-02-16 21:39:42 -0600

    implement card JS and added styling

--- js/custom.js
+++ js/custom.js
@@ -9,9 +9,30 @@
 
 function isPalindrome(word) {
     if (word == word.split('').reverse().join('')) {
-        alert(word + ' is palindrome.');
+        // alert(word + ' is palindrome.');
+        document.getElementById("card-body").innerHTML = word + " is a palindrome!";
     }
     else {
-        alert(word + ' is not palindrome.');
+        document.getElementById("card-body").innerHTML = word + " is not a palindrome!";
     }
 }
+
+
+$('.card').click(function() {
+  $('.card-body').toggleClass('close');
+  document.getElementById("word").value = "";
+});
+
+
+
+{/* <div class="col">
+                    <input id="word" type="text" class="form-control" placeholder="Enter word">
+                  </div> */}
+
+
+            //       <div class="row">
+            //       <div class="col">
+            //         <p id="output"></p>
+            //       </div>
+            //     </div>
+            //   </div>
